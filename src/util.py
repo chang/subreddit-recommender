48376e5057fac90a4fa6173e7cb9cfda292ed6c8
Eric <ericchang00@gmail.com>
2017-10-15 22:07:07 -0700

    - move helpers to subreddit_recommender.src.util - get rid of global dirname and filename variables

--- src/util.py
+++ src/util.py
@@ -1,3 +1,4 @@
+import json
 import os
 import os.path as op
 
@@ -96,3 +97,39 @@ def data_dir_file(file_name, subdir=''):
     if not op.exists(dirname):
         os.mkdir(dirname)
     return op.join(dirname, file_name)
+
+
+def data_dir_subreddit(*args, raw_data_dirname='reddit_raw'):
+    """Returns the subreddit data directory.
+
+    Input should be a tuple of the form (category, subcategory, subreddit),
+    or as separate arguments.
+    """
+    args_correct_length = len(args) == 3 or len(args) == 1
+    single_arg_correct_length = len(args[0]) == 3 if len(args) == 1 else True
+    if not (args_correct_length and single_arg_correct_length):
+        raise ValueError('Input should be a tuple of the form (category, subcategory, subreddit), or *args')
+
+    if len(args) == 1:
+        args = args[0]
+    category, subcategory, subreddit = args
+
+    reddit_data_dir = op.join(data_dir('raw'), raw_data_dirname)
+    elems = [reddit_data_dir] + [valid_subreddit_dirname(e) for e in args]
+    return op.join(*elems)
+
+
+def load_json(filename, subdir='raw'):
+    """Loads a .json by filename or path. Assumes the file is in data/raw."""
+    if not op.exists(filename):
+        filename = op.join(data_dir(subdir), filename)
+    with open(filename, 'r') as file:
+        subreddit_dict = json.load(file)
+    return subreddit_dict
+
+
+def valid_subreddit_dirname(s):
+    """Converts a subreddit name to a valid directory name.
+    (necessary since some subreddits have forward slashes in their names)
+    """
+    return s.replace('/r/', '').replace('/', '')
